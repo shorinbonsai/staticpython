cf48147b494e2a78c6e1c936e1cc7f2bc5550e68
James <shorinbonsai@gmail.com>
2024-03-15 00:05:22 -0400

    functional recursive generation

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 python src/main.py
+python server.py --dir public
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import os
 import shutil
+from markdown_blocks import markdown_to_html_node
+# from pathlib import Path
 
-
+dir_path_content = "./content"
+template_path = "./template.html"
 dir_path_static = "./static"
 dir_path_public = "./public"
 
@@ -19,6 +22,60 @@ def copy_files_recursive(source_dir_path, dest_dir_path):
         else:
             copy_files_recursive(from_path, dest_path)
 
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        if os.path.isfile(from_path):
+            if filename.endswith('.md'):
+                # Generate the corresponding HTML file
+                dest_path = os.path.join(dest_dir_path, filename.replace('.md', '.html'))
+                generate_page(from_path, template_path, dest_path)
+        else:
+            new_dest_dir_path = os.path.join(dest_dir_path, filename)
+            os.makedirs(new_dest_dir_path, exist_ok=True)
+            generate_pages_recursive(from_path, template_path, new_dest_dir_path)
+
+# def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+#     for filename in os.listdir(dir_path_content):
+#         from_path = os.path.join(dir_path_content, filename)
+#         dest_path = os.path.join(dest_dir_path, filename)
+#         if os.path.isfile(from_path):
+#             dest_path = Path(dest_path).with_suffix(".html")
+#             generate_page(from_path, template_path, dest_path)
+#         else:
+#             generate_pages_recursive(from_path, template_path, dest_path)
+
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
+
 def main():
     print("Deleting public dir...")
     if os.path.exists(dir_path_public):
@@ -26,6 +83,12 @@ def main():
 
     print("Copying static files to public dir...")
     copy_files_recursive(dir_path_static, dir_path_public)
+    print("Generating page...")
+    generate_pages_recursive(
+        dir_path_content,
+        template_path,
+        dir_path_public,
+    )
 
 
 main()
